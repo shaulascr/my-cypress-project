fe645d2cc9d5eb30ecc98e354b9c0b4c19d71b07
shaulascr <alya.roosyidah@gmail.com>
2025-05-08 10:24:31 +0700

    add report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     supportFile:"cypress/support/e2e.js",
     defaultCommandTimeout: 30000, //atur waktu timeout custom dl miliseconds
